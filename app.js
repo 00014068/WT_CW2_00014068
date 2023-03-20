df5982d5185f33e856eeac31ea5dc7020dbfc287
Marina Lee <marinalee@MacBook-Air-Marina.local>
2023-03-21 03:35:36 +0500

    build & style create page

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/create', (req, res) => {
+    res.render('create')
+})
+
 app.listen(8000, err => { 
     if (err) console.log(err)
 
