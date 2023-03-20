65a52a831d7b4fa2f9ad94ad0983afdfc68da899
Marina Lee <marinalee@MacBook-Air-Marina.local>
2023-03-21 02:47:39 +0500

    style layout

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express()
 
 app.set('view engine', 'pug')
 
+app.use('/static', express.static('public'))
+
 app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.listen(8000, err => {
+app.listen(8000, err => { 
     if (err) console.log(err)
 
     console.log('Server is running on port 8000...')
