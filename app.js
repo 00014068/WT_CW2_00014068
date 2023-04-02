f7afc42cc2767308622d90371910289fd7f9c759
Marina Lee <marinalee@MacBook-Air-Marina.local>
2023-04-02 22:36:44 +0500

    update operation

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { log } = require('console')
 const express = require('express')
 const app = express()
 
@@ -29,7 +30,7 @@ app.post('/create', (req,res) => {
             const notes = JSON.parse(data)
 
             notes.push({
-                id: id (),
+                id: id(),
                 title: title,
                 description: description,
             })
@@ -53,7 +54,7 @@ app.get('/notes', (req,res) => {
     })
 })
 
-app.get('/notes/:id', (req,res) => {
+const getNoteController = (page) => (req,res) => {
     const id = req.params.id
 
     fs.readFile('./data/notes.json', (err, data) => {
@@ -63,9 +64,35 @@ app.get('/notes/:id', (req,res) => {
 
         const note = notes.filter(note => note.id == id)[0]
 
-        res.render('detail', { note: note })
+        res.render(page, { note: note })
+    })
+};
+
+app.get('/notes/:id', getNoteController('detail'))
+app.get('/notes/:id/edit', getNoteController('edit-note'))
+
+app.post('/notes/:id/edit', (req, res) => {
+    const { title, description } = req.body
+    const id = req.params.id
+
+    fs.readFile('./data/notes.json', ( err, data) => {
+        if (err) throw err
+
+        const notes = JSON.parse(data)
+
+        const noteToUpdate = notes.find((note) => note.id === id);
+
+        noteToUpdate.title = title;
+        noteToUpdate.description = description;
+
+        fs.writeFile('./data/notes.json', JSON.stringify(notes), err => {
+            if (err) throw err
+
+            res.render('notes', { notes: notes })
+        })
     })
 })
+
 
 app.get('/:id/delete', (req, res) => {
     const id = req.params.id
@@ -84,6 +111,8 @@ app.get('/:id/delete', (req, res) => {
         })
     })
 })
+
+
 
 app.listen(8000, err => { 
     if (err) console.log(err)
